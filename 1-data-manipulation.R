e18c3213f250d18a25edd642b4538a3c95f69ba8
Matina Angelopoulou <matinang@gmail.com>
2016-03-13 13:55:21 +0100

    updating the initial country selection

--- 1-data-manipulation.R
+++ 1-data-manipulation.R
@@ -4,12 +4,12 @@ library(dplyr)
 # 1. Country Selection --------------------------------------------------------
 
 # read data
-all_indicators <- read_csv("World_Bank_Data_EU_1970_to_2015.csv")
+all_indicators <- read_csv("Data/world-bank/World_Bank_Data_Eur_allyrs.csv")
 
 # filter data
 all_indicators %>%
-  select(country_name = `<U+FEFF>Country Name`,
-         country_code = `Country Code`,
+  select(cntr_name = `<U+FEFF>Country Name`,
+         cntr_code = `Country Code`,
          series_name = `Series Name`,
          series_code = `Series Code`,
          YR2013 = `2013 [YR2013]`) %>%
@@ -18,8 +18,13 @@ all_indicators %>%
 
 # select the countries to be used for the analysis
 gni_pcap_2013$YR2013 <- as.numeric(gni_pcap_2013$YR2013)
-gni_pcap_2013 %>% filter(YR2013 > mean(YR2013)) %>% select(country_name)
-mean(gni_pcap_2013$YR2013)
+
+gni_pcap_2013[c(1,3:22,24:29),] %>% # removing Russia, Belarus and Ukraine
+  filter(YR2013 > mean(YR2013)) %>%
+  select(cntr_name, cntr_code) -> selected_cntr
+
+# verify that the mean is equal to the reported one
+mean(gni_pcap_2013[c(1,3:22,24:29),]$YR2013)
 
 # 2. Construction of the dataset ----------------------------------------------
 
