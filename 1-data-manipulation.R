5c5306d31f63974f4a936c1daa9691e957c52a4f
Matina Angelopoulou <matinang@gmail.com>
2016-03-13 13:56:32 +0100

    constructing the death database

--- 1-data-manipulation.R
+++ 1-data-manipulation.R
@@ -28,4 +28,24 @@ mean(gni_pcap_2013[c(1,3:22,24:29),]$YR2013)
 
 # 2. Construction of the dataset ----------------------------------------------
 
+# Selecting sub-populations for France, Germany and the UK
+selected_cntr %>%
+  # selecting the total population dataset for France
+  mutate(cntr_code = replace(cntr_code, cntr_code == "FRA", "FRATNP")) %>%
+  # taking into account West Germany only
+  mutate(cntr_name = replace(cntr_name, cntr_code == "DEU", "West Germany")) %>%
+  mutate(cntr_code = replace(cntr_code, cntr_code == "DEU", "DEUTW")) %>%
+  # taking into account England & Wales instead of the complete UK dataset
+  mutate(cntr_name = replace(cntr_name, cntr_code == "GBR", "England & Wales")) %>%
+  mutate(cntr_code = replace(cntr_code, cntr_code == "GBR", "GBRTENW")) ->
+  selected_pop
 
+lapply(selected_pop$cntr_code,
+       function(x){
+         read_table(
+           paste0("Data/human-mortality-database/Deaths_1x1/",
+                  x, ".Deaths_1x1.txt"), skip = 2,
+           col_types = cols(Year = "i", Age = "i",
+                            Female = "d", Male = "d", Total = "d")) %>%
+           mutate(cntr_code = as.character(x))
+         }) %>% rbind_all -> deathDB
